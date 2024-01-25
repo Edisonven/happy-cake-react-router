27464a07be1dd77e5f0e970f581174e4cf463064
Edison <aalevenegass@gmail.com>
2024-01-25 17:48:44 -0300

    Añadiendo precios en efecto hover a los pasteles

--- happy_cake/src/components/views/Home.jsx
+++ happy_cake/src/components/views/Home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
         <h1 className="home__title">
           Bienvenidos a <span className="home__sub-title">Happy Cake</span>
         </h1>
-        <img src="./src/assets/imgs/logo.jpg" alt="" className="home__logo__img" />
+        <img
+          src="./src/assets/imgs/logo.jpg"
+          alt=""
+          className="home__logo__img"
+        />
       </div>
       <div className="home__section">
         <h1>Pasteles</h1>
@@ -14,9 +18,11 @@ const Home = () => {
       <div className="home__cards">
         <div className="home__card">
           <img src="./src/assets/imgs/img_1.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$15.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_2.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$19.990</h1>
         </div>
         <div className="home__card">
           <img
@@ -24,9 +30,11 @@ const Home = () => {
             alt=""
             className="card__img"
           />
+          <h1 className="card__price">$18.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_4.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$13.950</h1>
         </div>
         <div className="home__card">
           <img
@@ -34,18 +42,23 @@ const Home = () => {
             alt=""
             className="card__img"
           />
+          <h1 className="card__price">$21.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_6.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$25.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_7.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$20.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_8.jpg" alt="" className="card__img" />
+          <h1 className="card__price">$11.990</h1>
         </div>
         <div className="home__card">
           <img src="./src/assets/imgs/img_9.jpg" alt="" className="card__img" />
+          <h1 className="card__price">9.990</h1>
         </div>
       </div>
     </div>
