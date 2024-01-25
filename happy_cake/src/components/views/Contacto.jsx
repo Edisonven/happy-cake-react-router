77437d9e98a26701d5c216060b69c1f9dc76b506
Edison <aalevenegass@gmail.com>
2024-01-25 13:16:35 -0300

    Estilando apartado contacto

--- happy_cake/src/components/views/Contacto.jsx
+++ happy_cake/src/components/views/Contacto.jsx
@@ -1,7 +1,48 @@
 const Contacto = () => {
   return (
-    <div>
-      <h1>Contacto</h1>
+    <div className="contacto__container">
+      <h1 className="contacto__title">
+        Cuentanos...¿En qué te podemos ayudar?
+      </h1>
+      <form className="contacto__form">
+        <h3 className="contacto__message">Mensaje</h3>
+        <textarea className="contacto__textarea" cols="30" rows="10"></textarea>
+      </form>
+      <form className="contacto__form contacto__form__contact">
+        <h3 className="contacto__message">Información de Contacto</h3>
+        <input
+          type="text"
+          placeholder="Ingrese su nombre"
+          className="input input__name"
+        />
+        <div className="form__section">
+          <input
+            type="email"
+            placeholder="Ingrese su correo"
+            className="input input__correo"
+          />
+          <input
+            type="text"
+            placeholder="Ingrese su dirección"
+            className="input input__direccion"
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Ingrese su ciudad"
+          className="input input__ciudad"
+        />
+        <input
+          type="number"
+          placeholder="Ingrese su teléfono"
+          className="input input__telefono"
+        />
+        <input
+          type="text"
+          placeholder="Ingrese Código postal"
+          className="input input__codigo-postal"
+        />
+      </form>
     </div>
   );
 };
