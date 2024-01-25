afc98733dad67c74ca807496e76d85b8be788900
Edison <aalevenegass@gmail.com>
2024-01-25 16:50:23 -0300

    Añadiendo logo

--- happy_cake/src/components/views/Home.jsx
+++ happy_cake/src/components/views/Home.jsx
@@ -1,9 +1,12 @@
 const Home = () => {
   return (
     <div className="home__container">
-      <h1 className="home__title">
-        Bienvenidos a <span className="home__sub-title">Happy Cake</span>
-      </h1>
+      <div className="home__title__container">
+        <h1 className="home__title">
+          Bienvenidos a <span className="home__sub-title">Happy Cake</span>
+        </h1>
+        <img src="./src/assets/imgs/logo.jpg" alt="" className="home__logo__img" />
+      </div>
       <div className="home__section">
         <h1>Pasteles</h1>
         <hr className="home__line" />
