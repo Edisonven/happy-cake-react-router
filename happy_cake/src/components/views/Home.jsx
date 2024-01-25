46885e1bac8505db9c1992af9fd72330ffa58754
Edison <aalevenegass@gmail.com>
2024-01-25 12:43:38 -0300

    Creando cards de pasteles

--- happy_cake/src/components/views/Home.jsx
+++ happy_cake/src/components/views/Home.jsx
@@ -4,6 +4,30 @@ const Home = () => {
       <h1 className="home__title">
         Bienvenidos a <span className="home__sub-title">Happy Cake</span>
       </h1>
+      <div className="home__section">
+        <h1>Pasteles</h1>
+        <hr className="home__line" />
+      </div>
+      <div className="home__cards">
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_1.jpg" alt="" className="card__img" />
+        </div>
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_2.jpg" alt="" className="card__img" />
+        </div>
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_3.webp" alt="" className="card__img" />
+        </div>
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_4.jpg" alt="" className="card__img" />
+        </div>
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_5.jpeg" alt="" className="card__img" />
+        </div>
+        <div className="home__card">
+          <img src="./src/assets/imgs/img_6.jpg" alt="" className="card__img" />
+        </div>
+      </div>
     </div>
   );
 };
